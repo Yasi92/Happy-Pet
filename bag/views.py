5f1dda90179e9c9c2ea0001816cd1665e31400e7
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-03-24 19:42:34 +0100

    Add add_to_bag view, add context.py to render items in bag

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,26 @@
-from audioop import reverse
-from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
-from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect, render
 from products.models import Product
 
 
 def view_bag(request):
     """ A view to return the bag contents page """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a items to the shopping bag """
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+
+    bag = request.session.get('bag', {})
+
+    
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)    
