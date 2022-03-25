8315b3f3044bb7804c007b8ebea78bf04276ecc3
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-03-25 09:24:41 +0100

    Add products with different size and color to the shopping bag and context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,8 +1,12 @@
-
+from curses import has_key
+from decimal import Decimal
+from statistics import quantiles
 from django.conf import settings
 
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+from products.models import Product
+
 from happy_pet.settings import FREE_DELIVERY_THRESHOLD
 from products.models import Product
 
@@ -13,17 +17,68 @@ def bag_contents(request):
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
- 
+    color = None
+    size = None
+
     for item_id, item_data in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += item_data * product.price
-        product_count += item_data
-        bag_items.append({
-            'item_id' : item_id,
-            'quantity' : item_data,
-            'product' : product,
-        })
-    
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id' : item_id,
+                'quantity' : item_data,
+                'product' : product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            # for item, data in item_data.items():
+            if 'items_by_filter' in item_data:
+                item_data = item_data
+                for filter, value in item_data.items():
+                
+                    size = item_data['items_by_filter']['size']
+                    color = item_data['items_by_filter']['color']
+                    quantity = item_data['items_by_filter']['quantity']
+                    total += quantity * product.price
+                    product_count += quantity
+
+                    bag_items.append({
+                    'item_id' : item_id,
+                    'quantity' : quantity,
+                    'product' : product,
+                    'size' : size,
+                    'color' : color,
+                })
+
+            elif 'items_by_size' in item_data:
+
+                for size, quantity in item_data['items_by_size'].items():
+                    quantity = quantity
+                    total += quantity * product.price
+                    product_count += quantity
+
+                    bag_items.append({
+                    'item_id' : item_id,
+                    'quantity' : quantity,
+                    'product' : product,
+                    'size' : size,
+            })
+
+
+            elif 'items_by_color' in item_data:
+                for color, quantity in item_data['items_by_color'].items():    
+
+                    quantity = quantity
+                    total += quantity * product.price
+                    product_count += quantity
+
+                    bag_items.append({
+                    'item_id' : item_id,
+                    'quantity' : quantity,
+                    'product' : product,
+                    'color' : color,
+            })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = settings.STANDARD_DELIVERY_PERCENTAGE/100 * total
--- bag/views.py
+++ bag/views.py
@@ -12,15 +12,55 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    color = None
+    
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']        
 
+    if 'product_color' in request.POST:
+        color = request.POST['product_color']
 
     bag = request.session.get('bag', {})
 
-    
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:
-        bag[item_id] = quantity
+    if size and color:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_filter']['size'] and color in bag[item_id]['items_by_filter']['color']:
+                bag[item_id]['items_by_filter']['size'] = size
+                bag[item_id]['items_by_filter']['color'] = color
+                bag[item_id]['items_by_filter']['quantity'] += quantity
+            else:
+           
+                    bag[item_id]['items_by_filter']['size'] = size
+                    bag[item_id]['items_by_filter']['color'] = color  
+                    bag[item_id]['items_by_filter']['quantity'] = quantity  
+        else:   
+            bag[item_id] = {'items_by_filter' : {'size' : size,
+                                                'color' : color,
+                                                'quantity' : quantity,
+                                                }}
+    elif size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size' : {size : quantity}}
+
+    elif color:
+        if item_id in list(bag.keys()):
+            if color in bag[item_id]['items_by_color'].keys():
+                bag[item_id]['items_by_color'][color] += quantity
+            else:
+                bag[item_id]['items_by_color'][color] = quantity
+        else:
+            bag[item_id] = {'items_by_color' : {color : quantity}}
+    else:        
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
-    return redirect(redirect_url)    
+    return redirect(redirect_url)
