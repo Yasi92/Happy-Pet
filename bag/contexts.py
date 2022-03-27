b5282681578b9ab2ae30c34237730084d3c59e51
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-03-27 12:24:12 +0200

    Fix a typo in context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,3 @@
-from curses import has_key
-from decimal import Decimal
-from statistics import quantiles
 from django.conf import settings
 
 from django.conf import settings
@@ -80,9 +77,9 @@ def bag_contents(request):
                     'color' : color,
             })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+    if total < FREE_DELIVERY_THRESHOLD:
         delivery = settings.STANDARD_DELIVERY_PERCENTAGE/100 * total
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+        free_delivery_delta = FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
@@ -95,7 +92,7 @@ def bag_contents(request):
         "product_count":product_count,
         "delivery":delivery,
         "free_delivery_delta":free_delivery_delta,
-        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
+        "free_delivery_threshold": FREE_DELIVERY_THRESHOLD,
         "grand_total":grand_total,
     }
 
