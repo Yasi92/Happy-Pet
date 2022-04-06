a1392678d8232be5d8652fc4c6a78a0683c73dba
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-04-06 14:59:40 +0200

    Add the functionality to add items to whish list, display them in wish list page

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.view_wishlist, name='wishlist'),
-    # path('add/<item_id>', views.add_to_bag, name='add_to_bag'),
+    path('add_to_wishlist/<item_id>', views.add_to_wishlist, name='add_to_wishlist'),
 
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,6 +1,58 @@
+from math import prod
 from django.shortcuts import render
+from audioop import reverse
+from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
+from django.contrib import messages
+from products.models import Product
+import wishlist
+
 
 # Create your views here.
 def view_wishlist(request):
-    """ A view to return the bag contents page """
-    return render(request, 'wishlist/wishlist.html')
+    """ A view to return the wishlist page """
+
+
+    wishlist_bag = request.session.get('wishlist_bag', {})
+    print(wishlist_bag)
+
+    products = []
+
+    for item, value in wishlist_bag.items():
+        product = get_object_or_404(Product, product_id=item)
+        products.append(product)
+
+    tempalte = 'wishlist/wishlist.html'
+    context = {
+        'products' : products,
+    }
+
+    return render(request, tempalte, context)
+
+
+
+
+
+
+def add_to_wishlist(request, item_id):
+    product = get_object_or_404(Product, product_id=item_id)
+    # redirect_url = request.POST.get('redirect_url')
+
+    wishlist_bag = request.session.get('wishlist_bag', {})
+
+
+    if item_id in list(wishlist_bag.keys()):
+
+        messages.success(request, f'{ product.name } Already exists in your wish list')
+
+    else:
+        wishlist_bag[item_id] = True
+        messages.success(request, f'Added { product.name } to your wish list')
+
+
+
+    request.session['wishlist_bag'] = wishlist_bag
+
+   
+
+    return redirect('wishlist')
+
