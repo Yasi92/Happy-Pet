bbfaed4bde4b2685a9d378590b60a477caf5c856
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-04-06 12:47:59 +0200

    Add wishlist app and render the wishlist page

--- happy_pet/settings.py
+++ happy_pet/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'bag',
     'profiles',
     'checkout',
+    'wishlist',
 
 
     # other
---
+++ wishlist/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def view_wishlist(request):
+    """ A view to return the bag contents page """
+    return render(request, 'wishlist/wishlist.html')
