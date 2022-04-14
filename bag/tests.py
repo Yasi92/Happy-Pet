6b53bd03665b1032ec25ca9d79f25eac93bad0a8
Yasi <79868623+Yasi92@users.noreply.github.com>
2022-04-14 21:48:11 +0200

    Add unit testing for urls in bag app

--- bag/tests.py
+++ bag/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from .views import view_bag, add_to_bag, adjust_bag, remove_from_bag
 
-# Create your tests here.
+
+# Testing urls are learned from (https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2)
+
+class TestUrls(TestCase):
+    
+    def test_view_bag(self):
+        url = reverse('bag')
+        self.assertEquals(resolve(url).func, view_bag)
+
+
+    def test_add_to_bag(self):
+        url = reverse('add_to_bag', args=[55])
+        self.assertEquals(resolve(url).func, add_to_bag)
+
+
+    def test_adjust_bag(self):
+        url = reverse('adjust_bag', args=[44])
+        self.assertEquals(resolve(url).func, adjust_bag)
+
+
+    def test_remove_bag(self):
+        url = reverse('remove_from_bag', args=[44])
+        self.assertEquals(resolve(url).func, remove_from_bag)
--- home/tests.py
+++ home/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .views import index
+
 
 class TestViews(TestCase):
 
@@ -7,3 +10,9 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
 
+
+class TestUrls(TestCase):
+
+    def test_index(self):
+        url = reverse('home')
+        self.assertEquals(resolve(url).func, index)
